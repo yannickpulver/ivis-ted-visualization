ed9e0cf6b723b7eef8635b29135802086896f324
Yannick Pulver <hi@yanu.me>
2018-05-27 12:19:09 +0200

    getting there

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-const canvasHeight = 600,
+const canvasHeight = 750,
     canvasHeightTiny = 300,
     canvasWidth = 700,
     canvasWidthTiny = 500;
@@ -15,11 +15,19 @@ const svgSentiment = d3.select("#canvas-sentiment").append("svg")
     .attr("width", canvasWidthTiny)
     .attr("height", canvasHeightTiny);
 
+// https://github.com/wbkd/d3-extended
+d3.selection.prototype.moveToFront = function () {
+    return this.each(function () {
+        this.parentNode.appendChild(this);
+    });
+};
+
 const margin = {top: 20, right: 20, bottom: 20, left: 20};
+const marginTiny = {top: 40, right: 5, bottom: 20, left: 5};
 const width = canvasWidth - margin.left - margin.right;
-const widthTiny = canvasWidthTiny - margin.left - margin.right;
+const widthTiny = canvasWidthTiny - marginTiny.left - marginTiny.right;
 const height = canvasHeight - margin.top - margin.bottom;
-const heightTiny = canvasHeightTiny - margin.top - margin.bottom;
+const heightTiny = canvasHeightTiny - marginTiny.top - marginTiny.bottom;
 
 let format = d3.format(",d");
 
@@ -30,6 +38,8 @@ let pack = d3.pack()
 let fullData = [];
 let currentData = [];
 let currentCategory = "";
+let currentRating = "";
+let isSelectRatingGenerated = false;
 
 function visualizeData() {
     d3.csv("data/kaggle/ted_main_with_sentiment.csv", function (d) {
@@ -65,12 +75,19 @@ let buildDots = function (data) {
 
     let packed = pack(root);
 
+
+    if (currentRating !== "") {
+        topValue = d3.max(data, function (d) {
+            return d.ratingArray === undefined ? 0 : d.ratingArray[currentRating + "Percentage"];
+        });
+    }
+
     displayDots(packed, topValue)
 };
 
 let displayDots = function (packed, topValue) {
     let color = d3.scaleLinear()
-        .domain([1, topValue])
+        .domain([0, topValue])
         .range(['#ffb0ae', '#e53025'])
         .interpolate(d3.interpolateHcl);
 
@@ -88,10 +105,11 @@ let displayDots = function (packed, topValue) {
             return "translate(" + d.x + "," + d.y + ")";
         })
         .on("mouseover", function (d) {
-            div.html(d.data.title)
+
+            div.html("<span class='tag'>" + d.data.title + "</span><br>" + d3.format(".2s")(d.data.views) + " views")
                 .style("opacity", .9)
                 .style("left", (d.x + "px"))
-                .style("top", ((d.y - 50) + "px"));
+                .style("top", ((d.y - d.r - 50) + "px"));
         })
         .on("mouseout", function (d) {
             div.style("opacity", 0);
@@ -107,7 +125,7 @@ let displayDots = function (packed, topValue) {
         .attr("class", "circle")
         .attr("r", 0)
         .style("fill", function (d) {
-            return color(d.value);
+            return color(currentRating !== "" ? d.data.ratingArray[currentRating + "Percentage"] : d.value);
         });
 
     circle.transition()
@@ -134,6 +152,9 @@ let displayBars = function (data) {
         return dataF;
     });
 
+    buildRatingSelect(dataFormed);
+
+
     var x = d3.scaleBand().rangeRound([0, widthTiny]).padding(0.1),
         y = d3.scaleLinear().rangeRound([heightTiny, 0]);
 
@@ -145,7 +166,7 @@ let displayBars = function (data) {
     var percentageScale = d3.scaleLinear().domain([0, sum]).rangeRound([0, 100]);
 
     var g = svgLines.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + marginTiny.left + "," + marginTiny.top + ")");
 
     var div = d3.select("#canvas-lines").append("div")
         .attr("class", "tooltip")
@@ -164,6 +185,7 @@ let displayBars = function (data) {
 
     svgLines.selectAll(".bar").remove();
 
+
     g.selectAll(".bar")
         .data(dataFormed)
         .enter().append("rect")
@@ -172,7 +194,12 @@ let displayBars = function (data) {
                 var e = document.getElementById("selected-category-rating");
                 e.textContent = d.key;
             }
-            return "bar" + (y(d.value) === 0 ? " highest" : "")
+
+            if (d.key === currentRating.toLowerCase()) {
+                return "bar current"
+            } else {
+                return "bar" + (y(d.value) === 0 ? " highest" : "")
+            }
         })
         .attr("x", function (d) {
             return x(d.key);
@@ -185,7 +212,7 @@ let displayBars = function (data) {
             return height - y(d.value);
         })
         .on("mouseover", function (d) {
-            div.html(percentageScale(d.value) + "%")
+            div.html("<span class='tag'>" + d.key + "</span><br>" + percentageScale(d.value) + "%")
                 .style("opacity", .9)
                 .style("left", (x(d.key) + x.bandwidth()) + "px")
                 .style("top", (y(d.value) - 20) + "px");
@@ -195,24 +222,32 @@ let displayBars = function (data) {
         });
 };
 
-let buildBars = function (data) {
+
+let buildRatingArray = function (data) {
     data.forEach(function (f) {
         if (f.ratings !== undefined) {
             let ratings = JSON.parse(f.ratings);
-            f.ratingArray = []
+            f.ratingArray = [];
 
             //Building Rating array
             var jsonData = {};
             jsonData._all = 0;
+
+            ratings.forEach(function (rating) {
+                jsonData._all += rating.count;
+            });
+
             ratings.forEach(function (rating) {
                 jsonData[rating.name.replace(/\W/g, '')] = rating.count;
-                jsonData._all += rating.count;
+                jsonData[rating.name.replace(/\W/g, '') + "Percentage"] = 100 / jsonData._all * rating.count;
             });
 
             f.ratingArray = jsonData;
         }
     });
+};
 
+let buildBars = function (data) {
     var categoryMap = d3.values(data).map(function (d) {
         return d.ratingArray
     });
@@ -265,11 +300,6 @@ let buildBars = function (data) {
             };
         })
         .entries(categoryMap);
-
-
-    console.log(categoryMatching);
-
-
     displayBars(categoryMatching);
 
 
@@ -279,7 +309,7 @@ let buildSentimentCurves = function (data) {
 
     let dataFormed = d3.values(data).map(function (d, i) {
         let niceObject = [];
-        niceObject.title = d.title
+        niceObject.title = d.title;
         let niceData = [];
         for (let j = 1; j <= 5; j++) {
             let dataF = {};
@@ -293,10 +323,10 @@ let buildSentimentCurves = function (data) {
 
     dataFormed = dataFormed.sort(function (a, b) {
         return d3.descending(+a.views, +b.views);
-    }).slice(0, 10);
+    }).slice(0, 30);
 
     var g = svgSentiment.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + marginTiny.left + "," + marginTiny.top + ")");
 
     var div = d3.select("#canvas-sentiment").append("div")
         .attr("class", "tooltip")
@@ -306,7 +336,7 @@ let buildSentimentCurves = function (data) {
         y = d3.scaleLinear().range([heightTiny, 0]).domain([1, 5]);
 
     var line = d3.line()
-        .curve(d3.curveBasis)
+    //.curve(d3.curveBasis)
         .x(function (d) {
             return x(d.sentimentIndicator);
         })
@@ -314,24 +344,27 @@ let buildSentimentCurves = function (data) {
             return y(d.sentimentValue);
         });
 
-    g.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    g.append("g")
-        .attr("class", "axis axis--y")
-        .call(d3.axisLeft(y))
-        .append("text")
-        .attr("fill", "#000");
-
     svgSentiment.selectAll(".sentimentValue").remove();
-
 
     var goodOldLine = g.selectAll(".sentimentValue")
         .data(dataFormed)
         .enter().append("g")
         .attr("class", "sentimentValue");
+
+
+    goodOldLine.selectAll("line-dot")
+        .data(function (d) {
+            return d.values;
+        })
+        .enter().append("circle")
+        .attr("r", 3.5)
+        .attr("cx", function (d) {
+            return x(d.sentimentIndicator);
+        })
+        .attr("cy", function (d) {
+            return y(d.sentimentValue);
+        })
+        .attr("class", "line-dot");
 
     goodOldLine.append("path")
         .attr("class", "line")
@@ -340,10 +373,9 @@ let buildSentimentCurves = function (data) {
         })
         .attr("fill", "none")
         .attr("stroke", "#d7d7d7")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 3)
+        .attr("stroke-width", 2)
         .on("mouseover", function (d) {
+            d3.select(this.parentNode).moveToFront();
             div.html(d.title)
                 .style("opacity", .9)
                 .style("left", (d3.mouse(this)[0] + "px"))
@@ -360,7 +392,24 @@ function processData(error, data) {
     fullData = data;
     buildCategorySelect(data);
 
+    buildRatingArray(data);
+
     rebuild(data);
+}
+
+function buildRatingSelect(data) {
+    if (isSelectRatingGenerated) return;
+    isSelectRatingGenerated = true;
+
+    let tags = {};
+    var selectObject = document.getElementById("select-rating");
+
+    data.forEach(function (x) {
+        let option = document.createElement("option");
+        option.text = x.key;
+
+        selectObject.add(option)
+    });
 }
 
 
@@ -394,14 +443,10 @@ function buildCategorySelect(data) {
     });
 }
 
-function buildSentimentCategory(data) {
-
-}
-
-
 function onChangeCategory() {
     var e = document.getElementById("select-category");
     currentCategory = e.options[e.selectedIndex].value;
+
     currentData = fullData.filter(function (d) {
         return d.tags.includes(currentCategory)
     });
@@ -409,13 +454,35 @@ function onChangeCategory() {
     var e = document.getElementById("selected-category-label");
     e.textContent = currentCategory;
 
-    console.log(currentData.length);
+    rebuild(currentData);
+}
 
+function onChangeRating() {
+    var e = document.getElementById("select-rating");
+    currentRating = e.options[e.selectedIndex].value;
+    currentRating = currentRating[0].toUpperCase() + currentRating.slice(1);
+
+    var label = document.getElementById("bubbles-sortby");
+    label.textContent = currentRating;
 
     rebuild(currentData);
 }
 
+function buildText(data) {
+
+    var dataFormed = Object.create(data);
+
+    dataFormed = dataFormed.sort(function (a, b) {
+        return d3.descending(+a.views, +b.views);
+    });
+
+    var e = document.getElementById("selected-category-mostviewed");
+    e.textContent = dataFormed[0].title;
+
+}
+
 function rebuild(data) {
+    buildText(data);
     buildDots(data);
     buildBars(data);
     buildSentimentCurves(data);
